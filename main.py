f2211b626963e5f66ee4ec13e7efa1b611f1aeb8
VarIzo <varfalomei.izoldin@yandex.ru>
2023-07-06 13:49:01 +0300

    fix

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import searchInsert
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     searchInsert.test()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
